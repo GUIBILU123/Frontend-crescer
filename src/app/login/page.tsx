467dcd9c5a8403cb0dbfcf2cc8a4f9294166e6e2
GUIBILU123 <135767989+GUIBILU123@users.noreply.github.com>
2024-11-30 14:48:17 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -46,7 +46,7 @@ const LoginPage = () => {
       try {
         setSubmitting(true);
 
-        const response = await axios.post("https://crescer-mong-api.vercel.app/api/auth/login", values);
+        const response = await axios.post("https://crescer-mong-api-nu.vercel.app/api/auth/login", values);
 
         if (response.status === 200) {
           localStorage.setItem("authToken", response.data.token);
